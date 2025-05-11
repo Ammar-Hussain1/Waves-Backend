772b5dccdfff1b81e233b88ee7b51ca57fc1ffc3
Muhammad Ammar Hussain <ammarhusssain1@gmail.com>
2025-05-11 22:27:48 +0500

    Fixed bug in flight class type option in get seats api

--- src/controllers/seatsController.js
+++ src/controllers/seatsController.js
@@ -11,8 +11,8 @@ export const getSeats = async (req, res) => {
         }
         const pool = await poolPromise;
         const result = await pool.request()
-        .input('flightID', sql.INT, flightID)
-        .input('flightClassType', sql.Int, flightClassType)
+        .input('flightID', sql.Int, flightID)
+        .input('flightClassType', sql.NVarChar, flightClassType)
         .query(`SELECT S.SeatID, S.FlightID, S.SeatNumber, S.SeatClass, S.IsBooked, FC.ClassName 
             FROM Seats S 
             INNER JOIN 
